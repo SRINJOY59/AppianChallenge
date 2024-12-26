1e7e67ea411ffa102562e1eab369ed5991c106b9
SRINJOY59 <srinjoydas566@gmail.com>
2024-12-26 12:53:18 +0530

    agents modified

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def main():
             return  
         
         add_blue_theme()
-        tmp_path = 'Aaadhar.pdf'
+        tmp_path = r'Test_PDFs\voter_card.pdf'
         st.markdown("### Welcome to the DocuParse Pro!")
         st.markdown("### Effortlessly upload, analyze, and extract insights from your documents with precision.")
         uploaded_file = st.file_uploader("Upload a PDF document", type=['pdf'])
